ad09d2db9c2e11660785032336675281999e0cea
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2025-02-08 01:32:12 +0330

    Update entrypoint.sh

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -12,7 +12,16 @@ echo 'PostgreSQL started'
 echo 'Running migrations...'
 python manage.py makemigrations
 python manage.py makemigrations authentication
+python manage.py makemigrations invoices
 python manage.py migrate
+
+# Add this block after PostgreSQL check
+echo 'Waiting for Redis...'
+while ! nc -z redis 6379; do
+    echo "Waiting for Redis to start... Host: redis, Port: 6379"
+    sleep 0.1
+done
+echo 'Redis started'
 
 echo 'Checking for admin account...'
 cat <<EOF | python manage.py shell
