a46f3ed5137f866e7fddef0345b22b83ec6444ce
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2025-02-08 01:34:21 +0330

    Update apps.py

--- django/invoices/apps.py
+++ django/invoices/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class InvoicesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'invoices'
+
+    def ready(self):
+        import os
+        if os.environ.get('RUN_MAIN'):
+            from core.celery import app as celery_app
+            __all__ = ("celery_app",)
