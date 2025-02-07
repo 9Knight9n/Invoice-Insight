55239d4c935f0ab1e8ce865933211f787e871831
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2025-02-07 20:32:35 +0330

    Update views.py

--- django/authentication/views.py
+++ django/authentication/views.py
@@ -3,13 +3,11 @@ from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from knox.views import LoginView as KnoxLoginView
 from .serializers import UserSerializer, AuthSerializer
-from subscription.serializers import CustomerSerializer
 from django.forms.models import model_to_dict
-from subscription.models import Customer
 
 
 class RegisterView(generics.CreateAPIView):
-    serializer_class = CustomerSerializer
+    serializer_class = UserSerializer
 
 
 class LoginView(KnoxLoginView):
@@ -22,12 +20,7 @@ class LoginView(KnoxLoginView):
         user = serializer.validated_data['user']
         login(request, user)
         response = super(LoginView, self).post(request, format=None)
-        customer = Customer.objects.get(user=user)
-        customer_dict = customer.__dict__
         response.data['username'] = user.username
-        response.data['id'] = customer_dict['id']
-        response.data['credit'] = customer_dict['credit']
-        del response.data['expiry']
         return response
 
 
