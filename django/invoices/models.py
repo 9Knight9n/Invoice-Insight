1ab6f42706159860a4119b2c3acd849f71921c05
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2025-02-10 20:39:06 +0330

    Update models.py

--- django/invoices/models.py
+++ django/invoices/models.py
@@ -1,5 +1,6 @@
 import math
 
+from django.contrib.auth.models import User
 from django.db import models
 from django.core.exceptions import ValidationError
 
@@ -74,3 +75,13 @@ class Invoice(models.Model):
     def save(self, *args, **kwargs):
         self.full_clean()
         super().save(*args, **kwargs)
+
+
+class Comment(models.Model):
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on Invoice {self.invoice.id}"
