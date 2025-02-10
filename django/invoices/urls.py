4452104c954e9983e1ffda465fc521916c828d8a
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2025-02-10 20:16:44 +0330

    Update urls.py

--- django/invoices/urls.py
+++ django/invoices/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UploadPDFView, JobStatusView, InvoiceDetailView
+from .views import UploadPDFView, JobStatusView, InvoiceDetailView, CreateCommentView
 
 urlpatterns = [
     path('upload/', UploadPDFView.as_view(), name='upload_pdf'),
     path('status/<int:invoice_id>/', JobStatusView.as_view(), name='job_status'),
     path('invoice/<int:invoice_id>/', InvoiceDetailView.as_view(), name='invoice_detail'),
+    path('invoice/<int:invoice_id>/comment/', CreateCommentView.as_view(), name='create_comment'),
 ]
