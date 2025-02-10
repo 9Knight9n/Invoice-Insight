50a72532de00d72998bf68de90d8d5bc7c0a097d
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2025-02-10 19:56:24 +0330

    Update utils.py

--- django/invoices/utils.py
+++ django/invoices/utils.py
@@ -22,19 +22,19 @@ def extract_text_from_pdf(pdf_file):
         temp_file.write(pdf_bytes.getvalue())
         temp_file_path = temp_file.name
 
-    # PyPDF2
-    pdf_reader = PdfReader(pdf_file)
-    pypdf2_text = ""
-    for page in pdf_reader.pages:
-        pypdf2_text += page.extract_text()
-    extracted_data["PyPDF2"] = pypdf2_text
+    # # PyPDF2
+    # pdf_reader = PdfReader(pdf_file)
+    # pypdf2_text = ""
+    # for page in pdf_reader.pages:
+    #     pypdf2_text += page.extract_text()
+    # extracted_data["PyPDF2"] = pypdf2_text
 
-    # PyMuPDF
-    pymupdf_text = ""
-    doc = fitz.open(stream=pdf_bytes, filetype="pdf")
-    for page in doc:
-        pymupdf_text += page.get_text("text")
-    extracted_data["PyMuPDF"] = pymupdf_text
+    # # PyMuPDF
+    # pymupdf_text = ""
+    # doc = fitz.open(stream=pdf_bytes, filetype="pdf")
+    # for page in doc:
+    #     pymupdf_text += page.get_text("text")
+    # extracted_data["PyMuPDF"] = pymupdf_text
 
     # pdfplumber
     pdfplumber_text = ""
@@ -43,15 +43,15 @@ def extract_text_from_pdf(pdf_file):
             pdfplumber_text += page.extract_text()
     extracted_data["pdfplumber"] = pdfplumber_text
 
-    # Camelot (for tables)
-    camelot_tables = []
-    try:
-        tables = camelot.read_pdf(temp_file_path, pages="all")
-        for table in tables:
-            camelot_tables.append(table.df.to_dict())
-    except Exception as e:
-        print(f"Error using Camelot: {e}")
-    extracted_data["Camelot"] = camelot_tables
+    # # Camelot (for tables)
+    # camelot_tables = []
+    # try:
+    #     tables = camelot.read_pdf(temp_file_path, pages="all")
+    #     for table in tables:
+    #         camelot_tables.append(table.df.to_dict())
+    # except Exception as e:
+    #     print(f"Error using Camelot: {e}")
+    # extracted_data["Camelot"] = camelot_tables
 
     # Tabula (for tables)
     tabula_tables = []
@@ -62,13 +62,13 @@ def extract_text_from_pdf(pdf_file):
         print(f"Error using Tabula: {e}")
     extracted_data["Tabula"] = tabula_tables
 
-    # PDFMiner
-    try:
-        pdf_bytes.seek(0)
-        pdfminer_text = extract_text(pdf_bytes)
-    except Exception as e:
-        pdfminer_text = f"Error using PDFMiner: {e}"
-    extracted_data["PDFMiner"] = pdfminer_text
+    # # PDFMiner
+    # try:
+    #     pdf_bytes.seek(0)
+    #     pdfminer_text = extract_text(pdf_bytes)
+    # except Exception as e:
+    #     pdfminer_text = f"Error using PDFMiner: {e}"
+    # extracted_data["PDFMiner"] = pdfminer_text
 
     return extracted_data
 
@@ -82,6 +82,11 @@ def call_llm_api_general(extracted_text):
         "api_url": "https://models.inference.ai.azure.com",
         "api_key": os.getenv("OPENAI_API_KEY")
     }
+    # llm_config = {
+    #     "name": "Meta-Llama-3.1-405B-Instruct",
+    #     "api_url": "https://api.sambanova.ai/v1",
+    #     "api_key": os.getenv("OPENAI_API_KEY")
+    # }
 
     # Initialize the OpenAI client
     client = OpenAI(
@@ -98,9 +103,10 @@ def call_llm_api_general(extracted_text):
         except the fields related to items in invoice, they will be extracted later on,
         just need fields that are related to the invoice features.
         return the result as JSON. No more explanation, only a JSON is enough.
+        provide whole JSON and do not truncate the json. 
         the json format should be list of objects each with two fields of 'Name' and 'Value' 
         'Name' is the name of the field in the invoice, 
-        'Value' is the value of the field in the invoice.
+        'Value' is the value of the field in the invoice. convert these 'Value' to string if they are not. 
         both 'Name' and 'Value' should be user readable and easy to understand.
         """
 
@@ -165,12 +171,17 @@ def call_llm_api_item_wise(extracted_text):
     # Define the question/prompt
     question = """
         Extract items and their fields provided in the invoice, 
-        return the result as JSON. No more explanation, only a JSON is enough.
+        return the result as JSON. No more explanation, only a JSON is enough. 
+        provide whole JSON and do not truncate the json. 
+        all values in the JSON must be converted to string if they are not. 
         the json format should be list of objects with one mandatory field named 'Item Name'
         and two optional field named 'HS Code' and 'Part Number', 
         'HS Code' field is the Harmonized System Code assigned to items in the invoice.
-        'Part Number' field is self explanatory.
+        'Part Number' field may also be referred as 'item Number' or 'item no' or 'part no' as well.
         assign null value for any of 'HS Code' or 'Part Number' if value not provided. 
+        on some invoices 'Item Name' might be same as 'Part Number' and that's ok, 
+        in cases like this when no 'Item Name' is provided prefer 'Part Number' 
+        than description to assign as 'Item Name'.
         add any other fields the invoice provide for each item as well, 
         name these fields as they are named in the invoice. 
         be careful not to add the total row as a item in invoice.
