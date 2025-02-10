560862fda85af8ba064f4a5f8779d8cffafc6154
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2025-02-10 20:38:43 +0330

    Update views.py

--- django/invoices/views.py
+++ django/invoices/views.py
@@ -3,7 +3,10 @@ from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Invoice
-from .tasks import process_pdf  # We'll create this task later
+from .serializers import CommentSerializer
+from .tasks import process_pdf
+from rest_framework import permissions
+
 
 class UploadPDFView(APIView):
     parser_classes = [MultiPartParser]
@@ -44,3 +47,19 @@ class InvoiceDetailView(APIView):
             return Response(data, status=status.HTTP_200_OK)
         except Invoice.DoesNotExist:
             return Response({"error": "Invoice not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+class CreateCommentView(APIView):
+    permission_classes = [permissions.AllowAny]  # Allow unauthenticated access
+
+    def post(self, request, invoice_id, *args, **kwargs):
+        try:
+            invoice = Invoice.objects.get(id=invoice_id)
+        except Invoice.DoesNotExist:
+            return Response({"error": "Invoice not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = CommentSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save(invoice=invoice)  # Pass the invoice object here
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
