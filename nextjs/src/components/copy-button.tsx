62ed44ccb08508abc774a7c809b227a4cde65176
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2025-02-16 17:43:01 +0330

    Update copy-button.tsx

--- nextjs/src/components/copy-button.tsx
+++ nextjs/src/components/copy-button.tsx
@@ -5,17 +5,38 @@ import Image from "next/image";
 const CopyButton = ({text} : {text: string}) => {
   const [isCopied, setIsCopied] = useState<boolean>(false);
 
-  const handleCopy = () => {
-    navigator.clipboard.writeText(text).then(() => {
+  const copyToClipboard = async () => {
+    if (!navigator.clipboard) {
+      const textArea = document.createElement("textarea");
+      textArea.value = text;
+      document.body.appendChild(textArea);
+      textArea.select();
+      try {
+        document.execCommand("copy");
+        setIsCopied(true);
+        setTimeout(() => {
+          setIsCopied(false);
+        }, 3000);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        document.body.removeChild(textArea);
+      }
+      return;
+    }
+    try {
+      await navigator.clipboard.writeText(text);
       setIsCopied(true);
       setTimeout(() => {
         setIsCopied(false);
       }, 3000);
-    });
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
-    <IconButton onClick={handleCopy}>
+    <IconButton onClick={copyToClipboard}>
       <Zoom in={isCopied} timeout={500}>
         <Image src="/images/check.svg" alt="Copied" width={16} height={16} style={{display: isCopied ? "block" : "none"}}/>
       </Zoom>
